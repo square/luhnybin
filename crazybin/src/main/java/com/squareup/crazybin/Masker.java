af9c4679b742ba2f13f11651224457418f83fa36
Bob Lee <crazybob@crazybob.org>
2011-11-15 11:21:02 -0800

    Cleaned up code.

--- crazybin/src/main/java/com/squareup/crazybin/Masker.java
+++ crazybin/src/main/java/com/squareup/crazybin/Masker.java
@@ -19,37 +19,63 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Identifies and masks suspected credit card numbers.
+ *
+ * @author Bob Lee (bob@squareup.com)
+ */
 public class Masker {
   public static void main(String[] args) throws IOException {
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in, "US-ASCII"));
+    Masker masker = new Masker();
+
+    // TODO: Stream instead of filtering line-by-line.
     String line;
-    while ((line = in.readLine()) != null) System.out.print(mask(line) + "\n");
+    while ((line = in.readLine()) != null) System.out.println(masker.mask(line));
   }
 
-  static final int MIN_LENGTH = 14;
-  static final int MAX_LENGTH = 16;
-  static final char MASK = 'X';
+  private static final int MIN_LENGTH = 14;
+  private static final int MAX_LENGTH = 16;
+  private static final char MASK = 'X';
+
+  /** Power of 2, > MAX_LENGTH. */
+  private static final int CAPACITY = MAX_LENGTH << 1;
+
+  static {
+    if (Integer.bitCount(CAPACITY) != 1) {
+      throw new AssertionError("CAPACITY isn't a power of 2.");
+    }
+  }
+
+  private int[] currentSums, nextSums;
+  private int end, length;
+
+  private Masker() {
+    this.currentSums = new int[CAPACITY];
+    this.nextSums = new int[CAPACITY];
+  }
 
   /**
    * Identifies card numbers and replaces the digits with {@literal 'X'}. Strings may contain
    * any character to the left and right of the number (including no character) and may contain
    * any combination of {@literal ' '} or {@literal '-'} within the number itself. Runs O(N).
+   * Not safe for concurrent use.
    */
-  public static String mask(String s) {
+  public String mask(String s) {
     if (s.length() < MIN_LENGTH) return s;
+    reset();
     char[] masked = null;
-    LuhnBuffer buffer = LuhnBuffer.get();
     for (int i = 0; i < s.length(); i++) {
       char c = s.charAt(i);
       if (digit(c)) {
-        buffer.push(c);
-        int matchingDigits = buffer.matchingDigits();
+        push(c);
+        int matchingDigits = matchingDigits();
         if (matchingDigits > 0) {
           if (masked == null) masked = s.toCharArray();
           mask(masked, i, matchingDigits);
         }
       } else if (!separator(c)) {
-        buffer.reset();
+        reset();
       }
     }
     return masked == null ? s : new String(masked);
@@ -65,8 +91,7 @@ public class Masker {
         masked[last] = MASK;
         count--;
       } else if (c == MASK) {
-        // This digit was masked by an overlapping match. Keep going in case the previous
-        // match was smaller.
+        // Already masked. Keep going in case the previous match was shorter.
         count--;
       } else {
         // assert separator(c)
@@ -85,84 +110,49 @@ public class Masker {
     return c == ' ' || c == '-';
   }
 
+  /** Sums of the individual digits after doubling 0-9. */
+  private static final int[] DOUBLED_AND_SUMMED = { 0, 2, 4, 6, 8, 1, 3, 5, 7, 9 };
+
+  void push(char digit) {
+    swapArrays();
+    int value = digit - '0';
+    accumulate(currentSums, value);
+    accumulate(nextSums, DOUBLED_AND_SUMMED[value]);
+    end = wrap(end + 1);
+    if (length < MAX_LENGTH) length++;
+  }
+
+  private void swapArrays() {
+    int[] temp = currentSums;
+    currentSums = nextSums;
+    nextSums = temp;
+  }
+
+  /** Stores value plus the previous sum in sums. */
+  private void accumulate(int[] sums, int value) {
+    sums[end] = sums[wrap(end - 1)] + value;
+  }
+
+  private void reset() {
+    // We don't care about end or existing values in the arrays.
+    length = 0;
+  }
+
+  private static int wrap(int index) {
+    return index & (CAPACITY - 1);
+  }
+
   /**
-   * A circular buffer that computes Luhn values for sequences of digits in O(1) time.
-   *
-   * See http://en.wikipedia.org/wiki/Luhn_algorithm
+   * Returns the maximum number of digits that look like a card number or {@literal 0} if none
+   * of the digits look like a card #.
    */
-  static class LuhnBuffer {
-
-    int[] currentSums, nextSums;
-    int end, length;
-
-    private LuhnBuffer() {
-      this.currentSums = new int[32]; // power of 2, at least 1 greater than MAX_LENGTH
-      this.nextSums = currentSums.clone();
-      reset();
+  private int matchingDigits() {
+    for (int length = this.length; length >= MIN_LENGTH; length--) {
+      int last = wrap(end - 1);
+      int base = currentSums[wrap(last - length)];
+      int sum = currentSums[last];
+      if ((sum - base) % 10 == 0) return length;
     }
-
-    private static final ThreadLocal<LuhnBuffer> localInstance = new ThreadLocal<LuhnBuffer>() {
-      @Override protected LuhnBuffer initialValue() {
-        return new LuhnBuffer();
-      }
-    };
-
-    static LuhnBuffer get() {
-      return localInstance.get().reset();
-    }
-
-    void push(char digit) {
-      // Luhn starts from the right with an unmodified digit.
-      int value = digit - '0';
-      int doubled = doubleAndSumDigits(value);
-
-      swapArrays();
-
-      accumulate(currentSums, value);
-      accumulate(nextSums, doubled);
-
-      end = wrap(end + 1);
-      if (length < MAX_LENGTH) length++;
-    }
-
-    private static int doubleAndSumDigits(int value) {
-      // assert 0 <= value <= 9
-      int result = value << 1;
-      return result > 9 ? result - 9 : result;
-    }
-
-    private void swapArrays() {
-      int[] temp = currentSums;
-      currentSums = nextSums;
-      nextSums = temp;
-    }
-
-    private void accumulate(int[] sums, int value) {
-      sums[end] = sums[wrap(end - 1)] + value;
-    }
-
-    LuhnBuffer reset() {
-      end = 1;
-      length = currentSums[0] = nextSums[0] = 0;
-      return this;
-    }
-
-    static int wrap(int index) {
-      return index & 31; // & buffer length - 1
-    }
-
-    /**
-     * Returns the maximum number of digits that look like a card number or {@literal 0} if none
-     * of the digits look like a card #.
-     */
-    int matchingDigits() {
-      for (int length = this.length; length >= MIN_LENGTH; length--) {
-        int last = wrap(end - 1);
-        int base = currentSums[wrap(last - length)];
-        int sum = currentSums[last];
-        if ((sum - base) % 10 == 0) return length;
-      }
-      return 0;
-    }
+    return 0;
   }
 }
