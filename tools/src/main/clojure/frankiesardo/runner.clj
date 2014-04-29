b21be67a6c0cd5eba65812c5812e8d96edc0704a
frankiesardo <fran.sardo@gmail.com>
2014-04-29 18:51:49 +0200

    First test passes
    Read and write to process input and output

--- tools/src/main/clojure/frankiesardo/runner.clj
+++ tools/src/main/clojure/frankiesardo/runner.clj
@@ -1,8 +1,14 @@
 (ns frankiesardo.runner
+  (:require
+   [clojure.java.io :as io])
   (:gen-class))
+
+
+(defn sanitize [line] line)
 
 (defn -main
   "Application entry point"
   [& args]
-  (println "Hey I'm MAINNNNN")
-  (comment Do app initialization here))
+  (with-open [rdr (io/reader *in*)]
+    (doseq [line (line-seq rdr)]
+      (println (sanitize line)))))
