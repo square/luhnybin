e42c879e313eb656db9141a0cdee33f6f03b1d10
frankiesardo <fran.sardo@gmail.com>
2014-04-29 18:22:05 +0200

    Setup clojure invoke

---
+++ tools/project.clj
@@ -0,0 +1,7 @@
+(defproject luhnybin-clj "0.1.0-SNAPSHOT"
+  :description "luhnybin challenge"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :source-paths ["src" "src/main/clojure"]
+  :main frankiesardo.runner)
---
+++ tools/src/main/clojure/frankiesardo/runner.clj
@@ -0,0 +1,8 @@
+(ns frankiesardo.runner
+  (:gen-class))
+
+(defn -main
+  "Application entry point"
+  [& args]
+  (println "Hey I'm MAINNNNN")
+  (comment Do app initialization here))
